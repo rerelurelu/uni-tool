ee1d2d969cf79e8a95a3fbd037756acf1df08d4a
zoniha <awaawapurin@gmail.com>
2021-07-29 09:35:48 +0900

    🎨 layout: Update

--- lib/screen/system.dart
+++ lib/screen/system.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:uni_tool/layout/color.dart';
+
 import 'package:uni_tool/layout/theme.dart';
 
 class SystemScreen extends StatelessWidget {
@@ -6,20 +9,38 @@ class SystemScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
-
     return Container(
-      height: screenSize.height,
-      width: screenSize.width,
       decoration: defaultColor,
       child: Center(
         child: Column(
           children: [
-            SizedBox(height: 100),
-            Container(
-              child: Text(
-                'このアプリについて',
-                style: TextStyle(fontSize: 18),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.all(18),
+              child: SettingsList(
+                backgroundColor: lightFog,
+                shrinkWrap: true,
+                sections: [
+                  SettingsSection(
+                    title: 'このアプリについて',
+                    titleTextStyle: TextStyle(fontSize: 18),
+                    tiles: [
+                      SettingsTile(
+                        title: 'バージョン',
+                        trailing: Text('1.0.0'),
+                      ),
+                      SettingsTile(
+                        title: 'アプリの使い方',
+                        trailing: Icon(Icons.arrow_forward_ios_rounded),
+                      ),
+                      SettingsTile(
+                        title: 'プライバシーポリシー',
+                        titleTextStyle: TextStyle(fontSize: 15),
+                        trailing: Icon(Icons.arrow_forward_ios_rounded),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
