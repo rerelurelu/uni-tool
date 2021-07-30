19eaa2e349e1ee56e2a2f99895c1ba61e844d4aa
zoniha <awaawapurin@gmail.com>
2021-07-30 22:41:02 +0900

    🎨 layout: global layout

---
+++ lib/layout/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:uni_tool/layout/color.dart';
+
+/* Fonts */
+final double formFontSize = 18;
+
+/* Colors */
+final Color textColor = teal;
+final Color formTextColor = sumi;
