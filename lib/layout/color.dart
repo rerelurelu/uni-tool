e3a9fea6f6d85705772c20e023ab29fefac36208
zoniha <awaawapurin@gmail.com>
2021-09-04 23:10:24 +0900

    😘 perf: Use Color instead of MaterialColor

--- lib/layout/color.dart
+++ lib/layout/color.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'package:uni_tool/layout/create_color.dart';
-
 /* White */
-final MaterialColor lightFog = createColor(0xFFCFD6E6);
-final MaterialColor fog = createColor(0xFFE7EFF9);
+const Color lightFog = Color(0xFFCFD6E6);
+const Color fog = Color(0xFFE7EFF9);
 
 /* Blue */
-final MaterialColor sky = createColor(0xFF7CC7E8);
-final MaterialColor lightPurple = createColor(0xFF93A5CE);
-final MaterialColor deepBlue = createColor(0xFF274060);
-final MaterialColor lapis = createColor(0xFF556DAC);
+const Color sky = Color(0xFF7CC7E8);
+const Color lightPurple = Color(0xFF93A5CE);
+const Color deepBlue = Color(0xFF274060);
+const Color lapis = Color(0xFF556DAC);
 
 /* Red */
-final MaterialColor pink = createColor(0xFFF19DB5);
-final MaterialColor coral = createColor(0xFFED8C72);
+const Color pink = Color(0xFFF19DB5);
+const Color coral = Color(0xFFED8C72);
 
 /* Green */
-final MaterialColor emerald = createColor(0xFF67B7B2);
-final MaterialColor teal = createColor(0xFF577979);
-final MaterialColor lightTeal = createColor(0xFF738c8c);
+const Color emerald = Color(0xFF67B7B2);
+const Color teal = Color(0xFF577979);
+const Color lightTeal = Color(0xFF738c8c);
 
 /* Black  */
-final MaterialColor sumi = createColor(0xFF8a8a8a);
+const Color sumi = Color(0xFF8a8a8a);
